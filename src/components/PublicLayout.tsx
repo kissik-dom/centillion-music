e57a54fb659f7383ea0e240f7f378c9c516ca2aa
kissik-dom <legal@kissikingdom.com>
2026-05-30 04:48:38 +0000

    fix: Wrap PublicLayout with SidebarProvider to fix useSidebar crash

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "./Header";
+import { SidebarProvider } from "./ui/sidebar";
 
 export function PublicLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 flex flex-col">
-        <Outlet />
-      </main>
-    </div>
+    <SidebarProvider defaultOpen={false}>
+      <div className="min-h-screen flex flex-col w-full">
+        <main className="flex-1 flex flex-col">
+          <Outlet />
+        </main>
+      </div>
+    </SidebarProvider>
   );
 }
